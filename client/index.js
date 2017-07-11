0d1eb7223d2dccaa12f6eec2fca124b9c0509283
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2017-07-10 23:25:51 -0400

    initial commit
    setup webpack 3 with react

---
+++ client/index.js
@@ -0,0 +1,12 @@
+/*
+    ./client/index.js
+    which is the webpack entry file
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(
+ <App/>,
+ document.getElementById('root')
+ );
